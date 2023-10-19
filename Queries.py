a27091f8440c59a8574cd9fe4e269ccba1ae0a14
darshanl-personal <131968487+darshanl-newt@users.noreply.github.com>
2023-10-20 00:32:09 +0530

    completed

--- Queries.py
+++ Queries.py
@@ -1,5 +1,11 @@
 insert_user_register_info = """
                             insert into login_users 
                             (name, email, password, registration_date)
-                            values (%s, %s, %s, CURRENT_TIMESTAMP)
+                            values (%s, %s, %s, CURRENT_TIMESTAMP);
                             """
+
+select_user_password = "select password from login_users where email = %s"
+
+select_user_email = "select email from login_users where email = %s"
+
+update_user_password = "update login_users set password = %s where name = %s and email = %s"
